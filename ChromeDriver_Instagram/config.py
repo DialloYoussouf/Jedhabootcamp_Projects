7af7d8914997dcced4e3114d13403859349478b0
DialloYoussouf <diallo.youssouf.salian@gmail.com>
2020-09-05 14:38:33 +0200

    update again

--- ChromeDriver_Instagram/config.py
+++ ChromeDriver_Instagram/config.py
@@ -1,11 +1,5 @@
-username = 'karamoko1982'
-password = 'T@m@teBox'
-
-#username = 'your_instagram_username'
-#password = 'your_instagram_password'
-
-user = 'Youssouf'
-psw = 'T%40n%40Corp5.0'
+username = 'your_instagram_username'
+password = 'your_instagram_password'
 
 # To acces my database
 user1 = 'user1'
